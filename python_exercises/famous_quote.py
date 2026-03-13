6e84919f89e7d6b0c3561d7fd926902bd645ad08
Cyber-Demon <demonc847@gmail.com>
2026-03-13 09:00:19 +0500

    Add missing Python exercise files to python_exercises/ folder

---
+++ python_exercises/famous_quote.py
@@ -0,0 +1,1 @@
+print(' David Goggins once said, "You are in danger of living a life so comfortable and soft, that you will die without ever realizing your true potential"')
---
+++ python_exercises/hello_world.py
@@ -0,0 +1,7 @@
+message = "Hello Python world!"
+
+print(message)
+
+message = "Hello Python Creash Course World!"
+
+print(message)
